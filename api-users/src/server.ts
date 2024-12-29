6967c95c9a79327e1bd4b864fe9212923ca54358
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-29 00:44:33 -0600

    feat: create entry point

---
+++ api-users/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes'
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+app.use('/users', userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
