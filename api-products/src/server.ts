a6417a32283356ddb7aaf3bfca2f5ad5c66bec36
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-29 01:15:42 -0600

    feat: import routes

--- api-products/src/server.ts
+++ api-products/src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import productRoutes from './routes/product.routes';
+import categoryRoutes from './routes/category.routes';
+
 const app = express();
 
 dotenv.config();
@@ -11,6 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3002;
+
+app.use('/products', productRoutes)
+app.use('/category', categoryRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
