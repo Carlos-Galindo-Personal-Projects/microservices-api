05ed4f86ee73281a0e1269b17cb080aa09e03347
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-30 00:54:11 -0600

    chore: set up middleware

---
+++ api-gateway/src/helpers/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const authMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const cookie = req.cookies["token"];
+        if (!cookie) {
+            res.status(401).json({ message: "Token no proporcionado" });
+            return;
+        }
+
+        const secret = process.env.JWT_SECRET;
+        if (!secret) {
+            res.status(500).json({ message: "JWT_SECRET no está definido" });
+            return;
+        }
+
+        const decoded = jwt.verify(cookie, secret) as JwtPayload;
+
+        req.user = decoded;
+
+        console.log(req.user);
+
+        next();
+    } catch (error: any) {
+        if (error.name === "JsonWebTokenError") {
+            res.status(401).json({ message: "Token inválido" });
+            return;
+        }
+        res.status(500).json({ message: "Error interno del servidor" });
+        return;
+    }
+};
+
+
+export default authMiddleware;
