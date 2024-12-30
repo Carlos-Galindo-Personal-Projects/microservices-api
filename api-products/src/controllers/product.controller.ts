4961df657f67812a59121b7710abcafa3e4f0b2e
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-29 23:11:47 -0600

    feat: create product controllers

---
+++ api-products/src/controllers/product.controller.ts
@@ -0,0 +1,182 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+const getProductsByCategory = async (req: Request, res: Response) => {
+    try {
+        const { categoryId } = req.params;
+
+        if (!categoryId) {
+            return res.status(400).json({ message: "El parametro categoryId es obligatorio" });
+        }
+
+        const products = await prisma.product.findMany({
+            where: {
+                categoryId: parseInt(categoryId)
+            },
+            select: {
+                id: true,
+                name: true,
+                price: true
+            }
+        });
+
+        if (products.length === 0) {
+            return res.status(204).json({ message: "No hay productos" });
+        }
+
+        return res.status(200).json(products);
+    } catch (error) {
+        return res.status(500).json({ error: "Error interno del servidor" });
+    }
+}
+
+const getProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({ message: "El parametro id es obligatorio" });
+        }
+
+        const product = await prisma.product.findFirst({
+            where: {
+                id: parseInt(id)
+            }
+        });
+
+        if (!product) {
+            return res.status(404).json({ message: "Producto no encontrado" });
+        }
+
+        return res.status(200).json(product);
+    } catch (error) {
+        return res.status(500).json({ error: "Error interno del servidor" });
+    }
+}
+
+const createProduct = async (req: Request, res: Response) => {
+    try {
+        let { name, price, description, amount, categoryId } = req.body;
+
+        if (!name || !price || !amount || !categoryId) {
+            return res.status(400).json({ message: "Todos los campos son obligatorios" });
+        }
+
+        price = parseFloat(price);
+        amount = parseInt(amount);
+
+        if (isNaN(price) || isNaN(amount)) {
+            return res.status(400).json({ message: "El precio y la cantidad deben ser números" });
+        }
+
+        await prisma.product.create({
+            data: {
+                name,
+                price: parseFloat(price),
+                description,
+                amount: parseInt(amount),
+                categoryId
+            }
+        });
+
+        return res.status(201).json({ message: "Producto creado exitosamente" });
+    } catch (error) {
+        return res.status(500).json({ error: "Error interno del servidor" });
+    }
+}
+
+const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({ message: "El parametro id es obligatorio" });
+        }
+
+        const product = await prisma.product.findFirst({
+            where: {
+                id: parseInt(id)
+            }
+        });
+
+        if (!product) {
+            return res.status(404).json({ message: "Producto no encontrado" });
+        }
+
+        let { price, amount } = req.body;
+
+        if (!price && !amount) {
+            return res.status(400).json({ message: "Debe enviar al menos un campo a actualizar" });
+        }
+
+        if (price) {
+            price = parseFloat(price);
+
+            if (isNaN(price)) {
+                return res.status(400).json({ message: "El precio debe ser un número" });
+            }
+        }
+
+        if (amount) {
+            amount = parseInt(amount);
+
+            if (isNaN(amount)) {
+                return res.status(400).json({ message: "La cantidad debe ser un número" });
+            }
+        }
+
+        await prisma.product.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                price: price ? price : product.price,
+                amount: amount ? amount : product.amount
+            }
+        });
+
+        return res.status(200).json({ message: "Producto actualizado exitosamente" });
+    } catch (error) {
+        return res.status(500).json({ error: "Error interno del servidor" });
+    }
+}
+
+const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({ message: "El parametro id es obligatorio" });
+        }
+
+        const product = await prisma.product.findFirst({
+            where: {
+                id: parseInt(id)
+            }
+        });
+
+        if (!product) {
+            return res.status(404).json({ message: "Producto no encontrado" });
+        }
+
+        await prisma.product.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+
+        return res.status(200).json({ message: "Producto eliminado exitosamente" });
+    } catch (error) {
+        return res.status(500).json({ error: "Error interno del servidor" });
+    }
+}
+
+export {
+    getProductsByCategory,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
--- api-products/src/routes/product.routes.ts
+++ api-products/src/routes/product.routes.ts
@@ -1,25 +1,26 @@
 import { Request, Response, Router } from "express";
+import { createProduct, deleteProduct, getProduct, getProductsByCategory, updateProduct } from "../controllers/product.controller";
 
 const router = Router();
 
-router.get('/:category', (req: Request, res: Response) => {
-
+router.get('/all/:categoryId', (req: Request, res: Response) => {
+    getProductsByCategory(req, res);
 })
 
 router.get('/:id', (req: Request, res: Response) => {
-
+    getProduct(req, res);
 })
 
 router.post('/', (req: Request, res: Response) => {
-
+    createProduct(req, res);
 })
 
 router.put('/:id', (req: Request, res: Response) => {
-
+    updateProduct(req, res);
 })
 
-router.delete('/', (req: Request, res: Response) => {
-
+router.delete('/:id', (req: Request, res: Response) => {
+    deleteProduct(req, res);
 })
 
 export default router;
