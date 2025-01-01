ac6afe865ed0bbaa11f64115e4706b61851a9fdf
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-01 00:40:36 -0600

    fix: set correct values

--- api-gateway-frontend/src/schemas/login.schema.ts
+++ api-gateway-frontend/src/schemas/login.schema.ts
@@ -8,8 +8,8 @@ const loginSchema = z.object({
         .max(64, { message: "El correo electrónico debe contener como máximo 64 caracteres" }),
     password: z
         .string({ required_error: "La contraseña es obligatoria" })
-        .min(12, { message: "El correo electrónico debe contener como mínimo 6 caracteres" })
-        .max(64, { message: "La contraseña debe contener como máximo 16 caracteres" })
+        .min(6, { message: "El correo electrónico debe contener como mínimo 6 caracteres" })
+        .max(16, { message: "La contraseña debe contener como máximo 16 caracteres" })
 });
 
 export default loginSchema;
