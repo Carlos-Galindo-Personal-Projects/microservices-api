24acee71a036026db1ab6a40dcca87199b1198fe
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-01 14:06:14 -0600

    feat: add test products

--- api-gateway-frontend/src/types/response.ts
+++ api-gateway-frontend/src/types/response.ts
@@ -1,3 +1,14 @@
 export interface ResponseMessage {
     message: string;
 }
+
+export interface ResponseProducts {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    amount: number;
+    category: {
+        name: string;
+    };
+}
