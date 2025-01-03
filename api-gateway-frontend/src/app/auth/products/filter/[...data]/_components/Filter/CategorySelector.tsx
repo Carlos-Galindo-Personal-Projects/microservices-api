72f272c190773df1d6a021d48aafecd1b2a7f2ca
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-02 23:55:03 -0600

    style: change bg and ring

--- api-gateway-frontend/src/app/auth/products/filter/[...data]/_components/Filter/CategorySelector.tsx
+++ api-gateway-frontend/src/app/auth/products/filter/[...data]/_components/Filter/CategorySelector.tsx
@@ -10,7 +10,7 @@ export default function CategorySelector({ categories, currentCategoryId, setCur
                 Category
             </label>
             <select
-                className="mt-1 block w-full border border-[#ffb300] dark:border-[#940533] rounded-lg px-3 py-2 bg-gray-50 dark:bg-gray-800 dark:focus:ring-[#940533] dark:focus:border-[#940533] focus:ring-[#ffb300] focus:border-[#ffb300] sm:text-sm"
+                className="mt-1 block w-full border border-[#ffb300] dark:border-[#940533] rounded-lg px-3 py-2 dark:bg-[#e8e8e8] dark:text-[#333333] text-[#e8e8e8] bg-[#333333] dark:focus:ring-[#940533] dark:focus:border-[#940533] focus:ring-[#ffb300] focus:border-[#ffb300] sm:text-sm"
                 name="categoryId"
                 id="categoryId"
                 value={currentCategoryId}
