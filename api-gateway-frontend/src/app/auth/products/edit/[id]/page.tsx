8be26370bca6041fd51627b0061dfb5144e2f98b
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-02 21:28:01 -0600

    style: add skeleton to edit form

--- api-gateway-frontend/src/app/auth/products/edit/[id]/page.tsx
+++ api-gateway-frontend/src/app/auth/products/edit/[id]/page.tsx
@@ -1,11 +1,13 @@
+import { Suspense } from "react";
 import Product from "./_components/Product";
+import FormEditSkeleton from "./_components/Skeleton/FormEditSkeleton";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
 
     return (
-        <div>
+        <Suspense fallback={<FormEditSkeleton />}>
             <Product id={id} />
-        </div>
+        </Suspense>
     );
 }
