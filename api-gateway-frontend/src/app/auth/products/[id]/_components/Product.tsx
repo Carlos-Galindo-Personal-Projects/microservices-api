390ea9bf6ccb4a9f99de74b044530678f80c04cf
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-02 23:28:14 -0600

    docs: give style to code

--- api-gateway-frontend/src/app/auth/products/[id]/_components/Product.tsx
+++ api-gateway-frontend/src/app/auth/products/[id]/_components/Product.tsx
@@ -22,12 +22,12 @@ export default async function Product({ id }: { id: string }) {
         msg = message.message
         return (
             <div className="flex flex-col my-36">
-            <div className="flex-grow container m-auto">
-                <h2 className="text-5xl font-bold dark:text-[#e8e8e8] text-[#333333] text-center mt-12">
-                    {msg}
-                </h2>
+                <div className="flex-grow container m-auto">
+                    <h2 className="text-5xl font-bold dark:text-[#e8e8e8] text-[#333333] text-center mt-12">
+                        {msg}
+                    </h2>
+                </div>
             </div>
-        </div>
         )
     }
 
