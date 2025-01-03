531916f396ece05cff2ffcbc52db50c246e6381e
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-02 21:51:04 -0600

    feat: add logout button

---
+++ api-gateway-frontend/src/app/auth/_components/LogoutButton.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { ResponseMessage } from "@/types/response"
+import { useRouter } from "next/navigation"
+
+export default function LogoutButton() {
+
+    const router = useRouter();
+
+    const handleClick = async () => {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}users/logout`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+
+            if(!response.ok) {
+                const msg: ResponseMessage = await response.json()
+                alert(msg.message)
+            }
+
+            alert("Sesión cerrada con éxito")
+            router.push("/no-auth/login")
+        } catch {
+            alert("Ha ocurrido un error al intentar cerrar sesión")
+        }
+    }
+
+    return (
+        <button
+            className="dark:bg-[#ffb300] bg-[#940533] dark:text-black text-white font-bold p-1 px-4 rounded"
+            onClick={handleClick}
+        >
+            Logout
+        </button>
+    )
+}
